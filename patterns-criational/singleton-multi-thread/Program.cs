8ccb58532c1e133a30e427534927c5e842bbc7d3
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-21 18:23:54 -0300

    Implement pattern

---
+++ patterns-criational/singleton-multi-thread/Program.cs
@@ -0,0 +1,12 @@
+﻿using singleton_multi_thread;
+
+Console.WriteLine("##### Padrão Singleton #####\n");
+Console.WriteLine("Tentando criar uma instância s1.");
+Singleton s1 = Singleton.Instance;
+Console.WriteLine("Tentando criar uma instância s2");
+Singleton s2 = Singleton.Instance;
+if (s1 == s2)
+    Console.WriteLine("Existem somemnte uma instância");
+else {
+    Console.WriteLine("Existyem instâncias diferentes (s1 e s2)");
+}
