48cf22757235884b5c05fd44f86580581e44ff0c
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-21 16:56:31 -0300

    Patterns prototype implementation

---
+++ patterns-criational/prototype-shallow-copy/Program.cs
@@ -0,0 +1,22 @@
+﻿using prototype_shallow_copy;
+
+Soldado soldado = new Soldado();
+soldado.Nome = "Silva";
+soldado.Arma = "Fuzil";
+
+Soldado soldado_clone1 = (Soldado)soldado.Clone();
+Soldado soldado_clone2 = (Soldado)soldado.Clone();
+
+//exibe valores do objeto original
+Console.WriteLine(">> Objeto Original");
+Console.WriteLine($"   {soldado.Nome} - {soldado.Arma}");
+
+//valores do objeto clone1
+Console.WriteLine(">> Objeto Clone1");
+Console.WriteLine($"   {soldado_clone1.Nome} - {soldado_clone1.Arma}");
+
+//valores do objeto clone2
+Console.WriteLine(">> Objeto Clone2");
+Console.WriteLine($"   {soldado_clone2.Nome} - {soldado_clone2.Arma}");
+
+Console.ReadLine();
---
+++ patterns-criational/prototype-shallow-copy/README.md
@@ -0,0 +1,4 @@
+# Prototype  
+
+O Prototype é um padrão de projeto criacional que permite copiar objetos existentes sem fazer seu código ficar dependente de suas classes.  
+https://refactoring.guru/pt-br/design-patterns/prototype
