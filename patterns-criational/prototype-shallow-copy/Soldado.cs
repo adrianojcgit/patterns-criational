01f070c1691cca1251c830b5d932f46da2e57c77
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-21 16:59:54 -0300

    add nullable

--- patterns-criational/prototype-shallow-copy/Soldado.cs
+++ patterns-criational/prototype-shallow-copy/Soldado.cs
@@ -2,8 +2,8 @@ namespace prototype_shallow_copy
 {
     public class Soldado : ICloneable
     {
-        public string Nome { get; set; }
-        public string Arma { get; set; }
+        public string? Nome { get; set; }
+        public string? Arma { get; set; }
         public Soldado() { }
         public Soldado(Soldado s)
         {
